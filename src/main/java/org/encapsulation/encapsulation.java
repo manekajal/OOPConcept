8041322eb0a720d42c9152cf8461bc7d92f3b586
kajal <manekajal2014@gmail.com>
2021-09-17 17:48:07 +0530

    Encapsulation

---
+++ src/main/java/org/encapsulation/encapsulation.java
@@ -0,0 +1,11 @@
+package org.encapsulation;
+
+class encapsulation {
+    public static void main(String[] args)
+    {
+        Student std = new Student();
+        std.setName("Kajal");
+        System.out.println(std.getName());
+
+    }
+}
